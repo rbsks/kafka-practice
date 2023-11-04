72f977c208cb8691bd9081368cb7541a9b54da6d
rbsks <rbsks147@daum.net>
2023-11-05 00:19:35 +0900

    kafka admin configuration 분리 및 gitignore 생성

---
+++ module-producer/src/main/java/com/example/moduleproducer/common/config/KafkaAdminConfiguration.java
@@ -0,0 +1,44 @@
+package com.example.moduleproducer.common.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaAdminConfiguration {
+
+    @Value("${bootstrapServers}")
+    private String bootstrapServers;
+
+    @Bean
+    public KafkaAdmin admin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic memberTopic() {
+        return TopicBuilder
+                .name("create_member")
+                .partitions(2)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic couponTopic() {
+        return TopicBuilder
+                .name("create_coupon")
+                .partitions(4)
+                .replicas(1)
+                .build();
+    }
+}
