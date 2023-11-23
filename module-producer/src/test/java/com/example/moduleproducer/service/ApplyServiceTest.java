48140cb06f1894b7758ad20aaf860891224f916f
HANPASS\rbsks147 <rbsks147@hanpass.com>
2023-11-23 13:43:06 +0900

    Chore: Add module-domain test package
    - module-domain 테스트 패키지 추가.
    - module-producer 쓸모없는 테스트 코드 제거
    - module-domain:module-rdbms CouponRepository count 메서드 제거

--- module-producer/src/test/java/com/example/moduleproducer/service/ApplyServiceTest.java
+++ module-producer/src/test/java/com/example/moduleproducer/service/ApplyServiceTest.java
@@ -1,6 +1,5 @@
 package com.example.moduleproducer.service;
 
-import com.example.modulecore.repository.CouponRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,26 +9,13 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-
 @SpringBootTest
 class ApplyServiceTest {
 
     @Autowired
     private ApplyService applyService;
     @Autowired
-    private CouponRepository couponRepository;
-    @Autowired
     private RedisTemplate<String, String> redisTemplate;
-
-    @Test
-    public void oneRequest() {
-        applyService.apply(1L);
-
-        long count = couponRepository.getCount();
-
-        assertThat(count).isEqualTo(1);
-    }
 
     @Test
     public void manyRequest() throws Exception {
@@ -50,10 +36,6 @@ class ApplyServiceTest {
         }
 
         countDownLatch.await();
-
-        long count = couponRepository.getCount();
-
-        assertThat(count).isEqualTo(100);
     }
 
     @Test
@@ -74,10 +56,6 @@ class ApplyServiceTest {
         }
 
         countDownLatch.await();
-
-        long count = couponRepository.getCount();
-
-        assertThat(count).isEqualTo(1);
     }
 
     @Test
