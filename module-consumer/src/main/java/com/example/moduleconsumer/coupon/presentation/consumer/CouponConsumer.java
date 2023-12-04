5c9f0198007f6cdfc6415243c2c44cd452ee0772
coby <rbsks147@hanpass.com>
2023-12-04 13:27:58 +0900

    Refactor: 컨슈머 커밋 코드 수정

--- module-consumer/src/main/java/com/example/moduleconsumer/coupon/presentation/consumer/CouponConsumer.java
+++ module-consumer/src/main/java/com/example/moduleconsumer/coupon/presentation/consumer/CouponConsumer.java
@@ -4,9 +4,11 @@ import com.example.moduleconsumer.coupon.application.CouponFacade;
 import com.example.moduleconsumer.member.presentation.dto.request.CreateMemberRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.CommitFailedException;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -37,9 +39,19 @@ public class CouponConsumer {
         int partitionId = record.partition();
         long offset = record.offset();
         String groupId = consumer.groupMetadata().groupId();
+        String topic = record.topic();
         log.info("offset: {}, partitionId: {}, groupId: {}", offset, partitionId, groupId);
-        couponFacade.createCoupon(memberId);
-        consumer.commitSync();
+        try {
+            couponFacade.createCoupon(memberId);
+            consumer.commitSync();
+        } catch (CommitFailedException e) {
+            log.error("Failed to commit offsets", e);
+            // 컨슈머의 처리시간이 max.poll.interval.ms보다 커서 리밸런싱 작업이 일어나거나
+            // session.timeout.ms 시간동안 heartbeat이 오지 않아 리밸런싱 작업이 일어난 경우
+            // seek 메서드를 이용해 실패했던 지점으로 offset 값을 되돌림. 컨슈머의 재처리 같은 경우는 정책을 잘 정해야할 필요가 있어보임
+            consumer.seek(new TopicPartition(topic, partitionId), offset);
+        }
+
     }
 
 }
